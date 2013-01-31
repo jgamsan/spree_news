faaa428eca3e47cb0540a97640b9319f5e5a369c
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-01-31 22:02:32 +0100

    Mas Modificaciones

--- app/controllers/spree/admin/posts_controller.rb
+++ app/controllers/spree/admin/posts_controller.rb
@@ -1,3 +1,13 @@
 class Spree::Admin::PostsController < Spree::Admin::ResourceController
+  def destroy
+    @post = Spree::Post.find(params[:id])
+    @post.delete
 
+    flash.notice = I18n.t('notice_messages.post_deleted')
+
+    respond_with(@object) do |format|
+      format.html { redirect_to collection_url }
+      format.js  { render_js_for_destroy }
+    end
+  end
 end
